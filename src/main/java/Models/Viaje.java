303b7bd5a62ee2c598a203bccdadecbea3528d30
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-17 15:53:17 -0600

    clase viaje creada

--- src/main/java/Models/Viaje.java
+++ src/main/java/Models/Viaje.java
@@ -1,4 +1,59 @@
 package Models;
+import Models.Cliente;
+import Models.Vehiculo;
 
 public class Viaje {
+    public String origen;
+    public String destino;
+    private Cliente cliente;
+    public Vehiculo vehiculo;
+    public float costo;
+
+    public Viaje(String origen, String destino, Cliente cliente, Vehiculo vehiculo, float costo) {
+        this.origen = origen;
+        this.destino = destino;
+        this.cliente = cliente;
+        this.vehiculo = vehiculo;
+        this.costo = costo;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
 }
